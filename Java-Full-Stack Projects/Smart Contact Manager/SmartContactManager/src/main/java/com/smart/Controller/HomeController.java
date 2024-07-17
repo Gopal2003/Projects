c4e23957b524f3697b6b8853f36b8a858f02232e
Gopal <gopalmittal2003@gmail.com>
2024-07-17 22:50:29 +0530

    User and Contact Table using the ER-Diagram

---
+++ Java-Full-Stack Projects/Smart Contact Manager/SmartContactManager/src/main/java/com/smart/Controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.smart.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.doa.UserRepository;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test() {
+		
+		User user = new User();
+		user.setName("Gopal Agarwal");
+		user.setEmail("gopalmittal234@gmail.com");
+		
+		userRepository.save(user);
+		return "Working";
+	}
+}
---
+++ Java-Full-Stack Projects/Smart Contact Manager/SmartContactManager/src/main/java/com/smart/doa/UserRepository.java
@@ -0,0 +1,9 @@
+package com.smart.doa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smart.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
+	
+}
