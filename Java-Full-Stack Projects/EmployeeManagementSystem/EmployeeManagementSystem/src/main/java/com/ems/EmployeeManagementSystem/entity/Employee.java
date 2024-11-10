aad7fed8b99ac0c0b96047715a43501776677a5b
Gopal <gopalmittal2003@gmail.com>
2024-11-10 17:19:44 +0530

    New Project Employee Management System

---
+++ Java-Full-Stack Projects/EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/EmployeeManagementSystem/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.ems.EmployeeManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+//Explicit Table Name. If Not mentioned, the table name will be same as the name of the class, all LowerCase.
+@Table(name="employees")
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Auto Increment the value of ID.
+    private Long id;
+
+    // Explicit Column Name. If Not specified, firstName would be the name of the column.
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id",nullable = false, unique = true)
+    private String email;
+
+}
