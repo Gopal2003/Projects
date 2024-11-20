70833c8d3c003ffffe9f39870b550f51f366f9a6
Gopal <gopalmittal2003@gmail.com>
2024-11-20 22:54:33 +0530

    navbar

--- Portfolio/react-portfolio/src/App.jsx
+++ Portfolio/react-portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import styles from './App.module.css'
+import Navbar from './components/NavBar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className={styles.App}>Hello World</div>
+      <div className={styles.App}>
+        <Navbar />
+      </div>
     </>
   )
 }
